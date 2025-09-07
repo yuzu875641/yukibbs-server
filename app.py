b010db06534beddbad3c0a45c86b79ee65934001
yuzu875641 <f.kokori85@gmail.com>
2025-09-07 11:48:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from datetime import datetime
 import threading
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 CORS(app)
 
 posts_by_id = {}
@@ -12,17 +12,17 @@ data_lock = threading.Lock()
 
 @app.route('/')
 def serve_index():
-    return send_from_directory('static', 'index.html')
+    with data_lock:
+        posts_list = [posts_by_id[post_id] for post_id in post_order]
+    
+    # 古い投稿を下に表示するため、逆順にする
+    sorted_posts = sorted(posts_list, key=lambda p: p['timestamp'])
+    
+    return render_template('index.html', posts=sorted_posts)
 
 @app.route('/static/<path:filename>')
 def serve_static(filename):
-    return send_from_directory('static', filename)
-
-@app.route('/posts', methods=['GET'])
-def get_posts():
-    with data_lock:
-        posts_list = [posts_by_id[post_id] for post_id in post_order]
-    return jsonify(posts_list)
+    return app.send_static_file(filename)
 
 @app.route('/posts', methods=['POST'])
 def create_post():
@@ -41,7 +41,8 @@ def create_post():
         posts_by_id[new_post_id] = new_post
         post_order.append(new_post_id)
 
-    return jsonify(new_post), 201
+    # SSRの場合、投稿後はリダイレクトで再描画する
+    return jsonify({'redirect': '/'}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
